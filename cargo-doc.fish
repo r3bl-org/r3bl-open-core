939ce4444251ed3da6f06c4147dab7e28a14dbfa
Nazmul Idris <nazmul@fasterlap.com>
2022-03-23 15:56:00 -0700

    Update fish scripts

--- cargo-doc.fish
+++ cargo-doc.fish
@@ -4,7 +4,6 @@
 # More info about cargo-watch: https://crates.io/crates/cargo-watch
 
 # 2. Make sure to install cargo-limit via `cargo install cargo-limit`.
-# More info about cargo-limit: https://crates.io/crates/cargo-limit
+# More info about carg-limit: https://crates.io/crates/cargo-limit
 
-# cargo doc --open
-cargo watch -x check -x 'ldoc -q --color always' -c -q
+cargo doc --open
---
+++ cargo-watch-macro-expand-one-test.fish
@@ -0,0 +1,23 @@
+#!/usr/bin/env fish
+
+# https://stackoverflow.com/a/47743269/2085356
+if test -z "$argv"
+    echo "Usage: "(set_color -o -u)"cargo-macro.fish "(set_color normal) \
+        (set_color yellow)"<test-name-fragment>" \
+        (set_color normal)
+    exit 1
+end
+
+# 1. Make sure to install cargo-watch via `cargo install cargo-watch`.
+# More info about cargo-watch: https://crates.io/crates/cargo-watch
+
+# 2. Make sure to install cargo-limit via `cargo install cargo-limit`.
+# More info about carg-limit: https://crates.io/crates/cargo-limit
+
+# More info about cargo test: https://doc.rust-lang.org/book/ch11-02-running-tests.html
+# 2 sets of options:
+# 1. cargo test --help      => these go to the cargo test command
+# 2. cargo test -- --help   => these go to the binary that is being tested
+
+# cargo watch -x check -x "ltest $argv --color always -q --message-format short  -- --nocapture" -c -q
+cargo watch -x "expand --test $argv"
