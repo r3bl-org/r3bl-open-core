4c0a48880f49fef026d917976579fe576b48ebea
Nazmul Idris <nazmul@fasterlap.com>
2022-06-12 15:10:37 -0700

    Update docs for lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -14,16 +14,28 @@
  limitations under the License.
 */
 
-//! This crate provides utility functions to do the following:
-//! 1. Non binary tree data structure that is safe to use across threads and supports parallel tree
-//!    walking & processing (inspired by [memory
-//!    arena](https://en.wikipedia.org/wiki/Region-based_memory_management).
-//! 2. Kotlin inspired [scope functions](https://kotlinlang.org/docs/scope-functions.html) that make
-//!    it easy to work w/ wrapped values (wrapped in [`std::sync::Arc`]<[`std::sync::RwLock`]>, or
-//!    [`Option`], etc).
-//! 3. Print colored text to the terminal. And create TUI (text user interface) applications,
-//!    similar to what you can do w/ [Ink, React, and TypeScript on
-//!    Node.js](https://github.com/r3bl-org/r3bl-ts-utils).
+//! This library provides utility functions:
+//!  1. Thread safe asynchronous Redux library (uses Tokio to run subscribers and middleware in
+//!     separate tasks). The reducer functions are run in sequence (not in Tokio tasks).
+//!  2. Declarative macros, and procedural macros (both function like and derive) to avoid
+//!     having to write lots of boilerplate code for many common (and complex) tasks.
+//!  3. Non binary tree data structure inspired by memory arenas, that is thread safe and
+//!     supports parallel tree walking.
+//!  4. Functions to unwrap deeply nested objects inspired by Kotlin scope functions.
+//!  5. Capabilities to make it easier to build TUIs (Text User Interface apps) in Rust. This
+//!     is currently experimental and is being actively developed.
+//!
+//! > 💡 To learn more about this library, please read how it was built on
+//! > [developerlife.com](https://developerlife.com):
+//! >
+//! > 1. https://developerlife.com/2022/02/24/rust-non-binary-tree/
+//! > 2. https://developerlife.com/2022/03/12/rust-redux/
+//! > 3. https://developerlife.com/2022/03/30/rust-proc-macro/
+//!
+//! > 💡 You can also read all the Rust content on
+//! > [developerlife.com here](https://developerlife.com/category/Rust/). Also, the equivalent
+//! > of this library is available for TypeScript and is called
+//! > [r3bl-ts-utils](https://github.com/r3bl-org/r3bl-ts-utils/).
 
 // Attach the following files to the library module.
 pub mod tree_memory_arena;
