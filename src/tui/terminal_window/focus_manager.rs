cb66df73e5836949f23120b63ff66d9e688f95e9
Nazmul Idris <nazmul@fasterlap.com>
2022-08-23 13:55:08 -0700

    Refactor focus_manager.rs, make it more ergnonomic

--- src/tui/terminal_window/focus_manager.rs
+++ src/tui/terminal_window/focus_manager.rs
@@ -57,15 +57,59 @@ where
   pub fn put(&mut self, name: &str, component: SharedComponent<S, A>) {
     self.components.insert(name.to_string(), component);
   }
+
   pub fn id_does_not_exist(&self, name: &str) -> bool { !self.components.contains_key(name) }
+
   pub fn get(&self, name: &str) -> Option<&SharedComponent<S, A>> { self.components.get(name) }
+
   pub fn get_has_focus(&self, has_focus: &HasFocus) -> Option<&SharedComponent<S, A>> {
     match has_focus.id {
       Some(ref id_has_focus) => self.get(id_has_focus),
       None => None,
     }
   }
+
   pub fn remove(&mut self, id: &str) -> Option<SharedComponent<S, A>> { self.components.remove(id) }
+
+  pub async fn route_to_focused_component(
+    &mut self, has_focus: &HasFocus, input_event: &TWInputEvent, state: &S,
+    shared_store: &SharedStore<S, A>,
+  ) -> CommonResult<EventPropagation> {
+    throws_with_return!({
+      // If component has focus, then route input_event to it. Return its propagation enum.
+      if let Some(shared_component_has_focus) = self.get_has_focus(has_focus) {
+        let result_event_propagation = shared_component_has_focus
+          .write()
+          .await
+          .handle_event(input_event, state, shared_store)
+          .await?;
+        return Ok(result_event_propagation);
+      };
+
+      // input_event not handled, propagate it.
+      EventPropagation::Propagate
+    });
+  }
+}
+
+#[macro_export]
+macro_rules! route_event_to_focused_component {
+  (
+    registry:     $arg_component_registry : expr,
+    has_focus:    $arg_has_focus          : expr,
+    input_event:  $arg_input_event        : expr,
+    state:        $arg_state              : expr,
+    shared_store: $arg_shared_store       : expr
+  ) => {
+    $arg_component_registry
+      .route_to_focused_component(
+        &$arg_has_focus,
+        $arg_input_event,
+        $arg_state,
+        $arg_shared_store,
+      )
+      .await
+  };
 }
 
 // ╭┄┄┄┄┄┄┄┄┄┄╮
