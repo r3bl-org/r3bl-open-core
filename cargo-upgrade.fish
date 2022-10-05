b87d78d5fa27bc033ba524ba06eb4099bf220daa
Nazmul Idris <nazmul@fasterlap.com>
2022-10-04 19:19:26 -0700

    Publish v0.8.1-macro

--- cargo-upgrade.fish
+++ cargo-upgrade.fish
@@ -3,6 +3,13 @@
 # 1. Make sure to install cargo-outdated via `cargo install --locked cargo-outdated`.
 # More info about cargo-outdated: https://crates.io/crates/cargo-outdated
 
-cargo outdated --workspace --verbose
-cargo upgrade --to-lockfile --verbose
-cargo update
+set -l folders . core macro redux tui
+
+for folder in $folders
+    pushd $folder
+    echo (set_color brmagenta)"≡ Upgrading '$folder' .. ≡"(set_color normal)
+    sh -c "cargo outdated --workspace --verbose"
+    sh -c "cargo upgrade --to-lockfile --verbose"
+    sh -c "cargo update"
+    popd
+end
