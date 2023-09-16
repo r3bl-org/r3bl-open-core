b9721f1eb6bb817f3e8f398f4f3187f9d4432bb8
Nazmul Idris <nazmul@fasterlap.com>
2023-09-15 19:59:32 -0700

    [tuify] Refactor CLI args
    Add subcommand support
    Add global opts
    
    More info:
    - https://rust-cli-recommendations.sunshowers.io/handling-arguments.html
    - https://docs.rs/clap/latest/clap/_derive/#overview
    
    Fixes: #128

--- tuify/src/main.rs
+++ tuify/src/main.rs
@@ -18,7 +18,7 @@
 use std::{io::{stdin, BufRead, Result},
           process::Command};
 
-use clap::{CommandFactory, Parser};
+use clap::{Args, CommandFactory, Parser, Subcommand};
 use crossterm::style::Stylize;
 use r3bl_rs_utils_core::*;
 use r3bl_tuify::*;
@@ -33,74 +33,116 @@ const SELECTED_ITEM_SYMBOL: char = '%';
 #[command(version)]
 #[command(next_line_help = true)]
 #[command(arg_required_else_help(true))]
-struct CliArgs {
-    /// Show TUI to allow you to select one or more options from a list, piped in via stdin 👉
-    #[arg(value_name = "mode", long, short = 's')]
-    selection_mode: SelectionMode,
+pub struct AppArgs {
+    #[clap(subcommand)]
+    command: CLICommand,
 
-    /// Each selected item is passed to this command as `%` and executed in your shell.
-    /// For eg: "echo %". Please wrap the command in quotes 💡
-    #[arg(value_name = "command", long, short = 'c')]
-    command_to_run_with_selection: String,
+    #[clap(flatten)]
+    global_opts: GlobalOpts,
+}
 
-    /// Optional maximum height of the list TUI (in rows)
-    #[arg(value_name = "height", long, short = 't')]
+#[derive(Debug, Args)]
+struct GlobalOpts {
+    /// Print debug output to log file (log.txt)
+    #[arg(long, short = 'l')]
+    enable_logging: bool,
+
+    /// Optional maximum height of the TUI (rows)
+    #[arg(value_name = "height", long, short = 'r')]
     tui_height: Option<usize>,
+
+    /// Optional maximum width of the TUI (columns)
+    #[arg(value_name = "width", long, short = 'c')]
+    tui_width: Option<usize>,
+}
+
+#[derive(Debug, Subcommand)]
+enum CLICommand {
+    /// Show TUI to allow you to select one or more options from a list, piped in via stdin 👉
+    SelectFromList {
+        /// Would you like to select one or more items?
+        #[arg(value_name = "mode", long, short = 's')]
+        selection_mode: SelectionMode,
+
+        /// Each selected item is passed to this command as `%` and executed in your shell.
+        /// For eg: "echo %". Please wrap the command in quotes 💡
+        #[arg(value_name = "command", long, short = 'c')]
+        command_to_run_with_selection: String,
+    },
+}
+
+fn get_bin_name() -> String {
+    let cmd = AppArgs::command();
+    cmd.get_bin_name().unwrap_or("this command").to_string()
 }
 
 fn main() -> Result<()> {
-    call_if_true!(TRACE, {
+    // If no args are passed, the following line will fail, and help will be printed
+    // thanks to `arg_required_else_help(true)` in the `CliArgs` struct.
+    let cli_args = AppArgs::parse();
+
+    let enable_logging = TRACE | cli_args.global_opts.enable_logging;
+
+    call_if_true!(enable_logging, {
+        log_debug(format!("cli_args {:?}", cli_args));
+    });
+
+    call_if_true!(enable_logging, {
         try_to_set_log_level(log::LevelFilter::Trace).ok();
         log_debug("Start logging...".to_string());
         log_debug(format!("og_size: {:?}", get_size()?).to_string());
     });
 
-    // If no args are passed, the following line will fail, and help will be printed
-    // thanks to `arg_required_else_help(true)` in the `CliArgs` struct.
-    let cli_args = CliArgs::parse();
-
-    call_if_true!(TRACE, {
-        log_debug(format!("cli_args {:?}", cli_args));
-    });
-
-    let bin_name = CliArgs::command();
-    let bin_name = bin_name.get_bin_name().unwrap_or("this command");
-
-    // macos has issues w/ stdin piped in.
-    // https://github.com/crossterm-rs/crossterm/issues/396
-    if cfg!(target_os = "macos") {
-        match (is_stdin_piped(), is_stdout_piped()) {
-            (StdinIsPiped, _) => {
-                show_error_stdin_pipe_does_not_work_on_macos();
+    match cli_args.command {
+        CLICommand::SelectFromList {
+            selection_mode,
+            command_to_run_with_selection,
+        } => {
+            // macos has issues w/ stdin piped in.
+            // https://github.com/crossterm-rs/crossterm/issues/396
+            if cfg!(target_os = "macos") {
+                match (is_stdin_piped(), is_stdout_piped()) {
+                    (StdinIsPiped, _) => {
+                        show_error_stdin_pipe_does_not_work_on_macos();
+                    }
+                    (_, StdoutIsPiped) => {
+                        show_error_do_not_pipe_stdout(get_bin_name().as_ref());
+                    }
+                    (StdinIsNotPiped, StdoutIsNotPiped) => {
+                        print_help()?;
+                    }
+                }
             }
-            (_, StdoutIsPiped) => {
-                show_error_do_not_pipe_stdout(bin_name);
-            }
-            (StdinIsNotPiped, StdoutIsNotPiped) => {
-                print_help()?;
+            // Linux works fine.
+            else {
+                match (is_stdin_piped(), is_stdout_piped()) {
+                    (StdinIsPiped, StdoutIsNotPiped) => {
+                        let tui_height = cli_args.global_opts.tui_height;
+                        let tui_width = cli_args.global_opts.tui_width;
+                        show_tui(
+                            selection_mode,
+                            command_to_run_with_selection,
+                            tui_height,
+                            tui_width,
+                            enable_logging,
+                        );
+                    }
+                    (StdinIsPiped, StdoutIsPiped) => {
+                        show_error_do_not_pipe_stdout(get_bin_name().as_ref());
+                    }
+                    (StdinIsNotPiped, StdoutIsPiped) => {
+                        show_error_need_to_pipe_stdin(get_bin_name().as_ref());
+                        show_error_do_not_pipe_stdout(get_bin_name().as_ref());
+                    }
+                    (StdinIsNotPiped, StdoutIsNotPiped) => {
+                        show_error_need_to_pipe_stdin(get_bin_name().as_ref());
+                    }
+                }
             }
         }
     }
-    // Linux works fine.
-    else {
-        match (is_stdin_piped(), is_stdout_piped()) {
-            (StdinIsPiped, StdoutIsNotPiped) => {
-                show_tui(cli_args);
-            }
-            (StdinIsPiped, StdoutIsPiped) => {
-                show_error_do_not_pipe_stdout(bin_name);
-            }
-            (StdinIsNotPiped, StdoutIsPiped) => {
-                show_error_need_to_pipe_stdin(bin_name);
-                show_error_do_not_pipe_stdout(bin_name);
-            }
-            (StdinIsNotPiped, StdoutIsNotPiped) => {
-                show_error_need_to_pipe_stdin(bin_name);
-            }
-        }
-    }
 
-    call_if_true!(TRACE, {
+    call_if_true!(enable_logging, {
         log_debug("Stop logging...".to_string());
     });
 
@@ -135,10 +177,16 @@ fn show_error_do_not_pipe_stdout(bin_name: &str) {
     println!("{msg}");
 }
 
-fn show_tui(cli_args: CliArgs) {
+fn show_tui(
+    selection_mode: SelectionMode,
+    command_to_run_with_selection: String,
+    tui_height: Option<usize>,
+    tui_width: Option<usize>,
+    enable_logging: bool,
+) {
     let lines = stdin().lock().lines().flatten().collect::<Vec<String>>();
 
-    call_if_true!(TRACE, {
+    call_if_true!(enable_logging, {
         log_debug(format!("lines: {:?}", lines));
     });
 
@@ -149,8 +197,8 @@ fn show_tui(cli_args: CliArgs) {
 
     // Get display size.
     let max_width_col_count: usize =
-        get_size().map(|it| it.col_count).unwrap_or(ch!(80)).into();
-    let max_height_row_count: usize = cli_args.tui_height.unwrap_or(5);
+        tui_width.unwrap_or(get_size().map(|it| it.col_count).unwrap_or(ch!(80)).into());
+    let max_height_row_count: usize = tui_height.unwrap_or(5);
 
     // Actually get input from the user.
     let selected_items = {
@@ -158,19 +206,18 @@ fn show_tui(cli_args: CliArgs) {
             lines,
             max_height_row_count,
             max_width_col_count,
-            cli_args.selection_mode,
+            selection_mode,
         );
         convert_user_input_into_vec_of_strings(it)
     };
 
-    call_if_true!(TRACE, {
+    call_if_true!(enable_logging, {
         log_debug(format!("selected_items: {:?}", selected_items));
     });
 
     for selected_item in selected_items {
-        let actual_command_to_run = &cli_args
-            .command_to_run_with_selection
-            .replace(SELECTED_ITEM_SYMBOL, &selected_item);
+        let actual_command_to_run =
+            &command_to_run_with_selection.replace(SELECTED_ITEM_SYMBOL, &selected_item);
         execute_command(actual_command_to_run);
     }
 }
@@ -204,7 +251,7 @@ fn execute_command(cmd_str: &str) {
 
 /// Programmatically prints out help.
 pub fn print_help() -> Result<()> {
-    let mut cmd = CliArgs::command();
+    let mut cmd = AppArgs::command();
     cmd.print_help()?;
     Ok(())
 }
