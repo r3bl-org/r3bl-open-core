4827727fdf9df7b9f4204749f78aebfe2699aa91
Nazmul Idris <nazmul@fasterlap.com>
2023-10-17 11:40:30 -0700

    [core] Remove dependency on ansi_term
    https://rustsec.org/advisories/RUSTSEC-2021-0139.html

--- src/utils/type_utils.rs
+++ src/utils/type_utils.rs
@@ -18,4 +18,6 @@
 
 /// Mimics the typeof operator in JavaScript.
 /// <https://stackoverflow.com/a/58119924/2085356>
-pub fn type_of<T>(_: &T) -> String { std::any::type_name::<T>().to_string() }
+pub fn type_of<T>(_: &T) -> String {
+    std::any::type_name::<T>().to_string()
+}
