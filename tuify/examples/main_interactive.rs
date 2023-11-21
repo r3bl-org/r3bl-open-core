79133e95ff8ccb6f29ed7f5f638c3b0b7a3e2dfc
Nadia Idris <nadia@fasterlap.com>
2023-11-21 14:15:03 -0800

    [tuify][examples] Add quiz game example (#223)

--- tuify/examples/main_interactive.rs
+++ tuify/examples/main_interactive.rs
@@ -20,6 +20,8 @@ use std::io::Result;
 use r3bl_ansi_color::{AnsiStyledText, Color as RColor, Style as RStyle};
 use r3bl_rs_utils_core::*;
 use r3bl_tuify::*;
+mod single_select_quiz_game;
+use single_select_quiz_game::main as single_select_quiz_game;
 
 fn print_header(msg: &str) {
     AnsiStyledText {
@@ -91,6 +93,7 @@ fn main() -> Result<()> {
             "Single select, 13 items, viewport height = 5";
         const SINGLE_SELECT_2_ITEMS_VPH_5: &str =
             "Single select, 2 items, viewport height = 5";
+        const SINGLE_SELECT_QUIZ_GAME: &str = "Single select, quiz game";
 
         // Add tuify to select which example to run.
         let maybe_user_input = select_from_list(
@@ -101,12 +104,13 @@ fn main() -> Result<()> {
                 MULTIPLE_SELECT_2_ITEMS_VPH_5,
                 SINGLE_SELECT_13_ITEMS_VPH_5,
                 SINGLE_SELECT_2_ITEMS_VPH_5,
+                SINGLE_SELECT_QUIZ_GAME,
             ]
             .iter()
             .map(|it| it.to_string())
             .collect(),
-            5,  /* whatever*/
-            80, /* whatever*/
+            6,  /* height of the tuify component */
+            80, /* width of the tuify component */
             SelectionMode::Single,
             StyleSheet::default(),
         );
@@ -130,8 +134,7 @@ fn main() -> Result<()> {
                                 style,
                                 multi_select_instructions,
                             );
-                        } else if *user_input
-                            == MULTIPLE_SELECT_2_ITEMS_VPH_5.to_string()
+                        } else if *user_input == MULTIPLE_SELECT_2_ITEMS_VPH_5.to_string()
                         {
                             multiple_select_2_items_vph_5(
                                 max_height_row_count,
@@ -139,8 +142,7 @@ fn main() -> Result<()> {
                                 style,
                                 multi_select_instructions,
                             );
-                        } else if *user_input
-                            == SINGLE_SELECT_13_ITEMS_VPH_5.to_string()
+                        } else if *user_input == SINGLE_SELECT_13_ITEMS_VPH_5.to_string()
                         {
                             // Single select.
                             single_select_13_items_vph_5(
@@ -148,14 +150,16 @@ fn main() -> Result<()> {
                                 max_width_col_count,
                                 style,
                             );
-                        } else if *user_input
-                            == SINGLE_SELECT_2_ITEMS_VPH_5.to_string()
+                        } else if *user_input == SINGLE_SELECT_2_ITEMS_VPH_5.to_string() 
                         {
                             single_select_2_items_vph_5(
                                 max_height_row_count,
                                 max_width_col_count,
                                 style,
                             );
+                        } else if *user_input == SINGLE_SELECT_QUIZ_GAME.to_string() 
+                        {
+                            let _ = single_select_quiz_game();
                         } else {
                             println!("User did not select anything")
                         }
