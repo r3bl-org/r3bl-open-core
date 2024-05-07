6988739058e000ee89ac992b791f47e77d143bef
Nazmul Idris <nazmul@fasterlap.com>
2024-05-06 19:52:51 -0500

    v0.5.2-terminal_async
    Fixes: #322

--- terminal_async/src/public_api/tracing_setup.rs
+++ terminal_async/src/public_api/tracing_setup.rs
@@ -16,31 +16,24 @@
  */
 
 use crate::SharedWriter;
-use crossterm::style::Stylize;
-use miette::IntoDiagnostic;
-use std::{path::PathBuf, str::FromStr};
-use tracing::info;
-use tracing_subscriber::fmt::writer::MakeWriterExt;
+use std::{io, path::PathBuf, str::FromStr};
+use tracing_core::LevelFilter;
+use tracing_subscriber::{
+    layer::SubscriberExt, registry::LookupSpan, util::SubscriberInitExt, Layer,
+};
 
 /// Fields:
-/// - `writers`: Vec<[tracing_writer_config::Writer]> - Zero or more writers to use for
+/// - `writers`: Vec<[WriterArg]> - Zero or more writers to use for
 ///   tracing.
 /// - `level`: [tracing::Level] - The log level to use for tracing.
 /// - `tracing_log_file_path_and_prefix`: [String] - The file path and prefix to use for
 ///   the log file. Eg: `/tmp/tcp_api_server` or `tcp_api_server`.
 #[derive(Clone)]
 pub struct TracingConfig {
-    pub writers: Vec<tracing_writer_config::Writer>,
+    pub writers: Vec<WriterArg>,
     pub level: tracing::Level,
     pub tracing_log_file_path_and_prefix: String,
     pub preferred_display: DisplayPreference,
-}
-
-#[derive(Clone)]
-pub enum DisplayPreference {
-    Stdout,
-    Stderr,
-    SharedWriter(SharedWriter),
 }
 
 mod tracing_config_impl {
@@ -51,16 +44,61 @@ mod tracing_config_impl {
         /// [DisplayPreference] and a file.
         pub fn new(preferred_display: DisplayPreference) -> Self {
             Self {
-                writers: vec![
-                    tracing_writer_config::Writer::File,
-                    tracing_writer_config::Writer::Stdout,
-                ],
+                writers: vec![WriterArg::File, WriterArg::Stdout],
                 level: tracing::Level::DEBUG,
                 tracing_log_file_path_and_prefix: "tracing_log_file_debug.log".to_string(),
                 preferred_display,
             }
         }
+
+        pub fn get_level_filter(&self) -> LevelFilter {
+            match self.level {
+                tracing::Level::ERROR => LevelFilter::ERROR,
+                tracing::Level::WARN => LevelFilter::WARN,
+                tracing::Level::INFO => LevelFilter::INFO,
+                tracing::Level::DEBUG => LevelFilter::DEBUG,
+                tracing::Level::TRACE => LevelFilter::TRACE,
+            }
+        }
     }
+}
+
+/// Use to parse the command line arguments (provided by `clap` crate.
+#[derive(Clone, Debug, PartialEq)]
+pub enum WriterArg {
+    Stdout,
+    File,
+    None,
+}
+
+/// Handle converting parsed command line arguments (via `clap` crate) into a [WriterArg].
+/// Note - this is an intermediate representation (IR), which is ultimately converted into
+/// [WriterConfig] by [writer_config_impl::from] before it used in the rest of the system.
+pub mod writer_arg_impl {
+    use super::*;
+
+    /// The `clap` crate parses this into a string. This function convert it into a
+    /// [WriterArg].
+    impl FromStr for WriterArg {
+        type Err = String;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            match s {
+                "stdout" => Ok(WriterArg::Stdout),
+                "file" => Ok(WriterArg::File),
+                "none" => Ok(WriterArg::None),
+                "" => Ok(WriterArg::None),
+                _ => Err(format!("{} is not a valid tracing writer", s)),
+            }
+        }
+    }
+}
+
+#[derive(Clone)]
+pub enum DisplayPreference {
+    Stdout,
+    Stderr,
+    SharedWriter(SharedWriter),
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -68,150 +106,149 @@ pub enum WriterConfig {
     Display,
     File,
     DisplayAndFile,
-    None,
 }
 
-impl From<&Vec<tracing_writer_config::Writer>> for WriterConfig {
-    fn from(writers: &Vec<tracing_writer_config::Writer>) -> Self {
-        let contains_file_writer = writers.contains(&tracing_writer_config::Writer::File);
-        let contains_stdout_writer = writers.contains(&tracing_writer_config::Writer::Stdout);
+pub mod writer_config_impl {
+    use super::*;
+
+    type DynLayer<S> = dyn Layer<S> + Send + Sync + 'static;
+
+    pub fn from(writers: &[WriterArg]) -> Option<WriterConfig> {
+        let contains_file_writer = writers.contains(&WriterArg::File);
+        let contains_stdout_writer = writers.contains(&WriterArg::Stdout);
         match (contains_file_writer, contains_stdout_writer) {
-            (true, true) => WriterConfig::DisplayAndFile,
-            (true, false) => WriterConfig::File,
-            (false, true) => WriterConfig::Display,
-            (false, false) => WriterConfig::None,
+            (true, true) => Some(WriterConfig::DisplayAndFile),
+            (true, false) => Some(WriterConfig::File),
+            (false, true) => Some(WriterConfig::Display),
+            (false, false) => None,
+        }
+    }
+
+    impl WriterConfig {
+        fn create_fmt<S>() -> tracing_subscriber::fmt::Layer<
+            S,
+            tracing_subscriber::fmt::format::DefaultFields,
+            tracing_subscriber::fmt::format::Format<tracing_subscriber::fmt::format::Compact, ()>,
+        >
+        where
+            S: tracing_core::Subscriber,
+            for<'a> S: LookupSpan<'a>,
+        {
+            tracing_subscriber::fmt::layer::<S>()
+                .compact()
+                .without_time()
+                .with_thread_ids(true)
+                .with_thread_names(false)
+                .with_target(false)
+                .with_file(false)
+                .with_line_number(false)
+                .with_ansi(true)
+        }
+
+        /// This erases the concrete type of the writer, and returns a boxed layer. This is
+        /// useful for composition of layers. There's more info in the docs
+        /// [here](https://docs.rs/tracing-subscriber/latest/tracing_subscriber/layer/index.html#runtime-configuration-with-layers).
+        pub fn layer_display<S>(
+            self,
+            level_filter: LevelFilter,
+            preferred_display: DisplayPreference,
+        ) -> miette::Result<Option<Box<DynLayer<S>>>>
+        where
+            S: tracing_core::Subscriber,
+            for<'a> S: LookupSpan<'a>,
+        {
+            // Shared configuration regardless of where logs are output to.
+            let fmt_layer = Self::create_fmt::<S>();
+
+            // Configure the writer based on the desired log target, and return it.
+            Ok(match self {
+                WriterConfig::DisplayAndFile | WriterConfig::Display => match preferred_display {
+                    DisplayPreference::Stdout => Some(Box::new(
+                        fmt_layer.with_writer(io::stdout).with_filter(level_filter),
+                    )),
+                    DisplayPreference::Stderr => Some(Box::new(
+                        fmt_layer.with_writer(io::stderr).with_filter(level_filter),
+                    )),
+                    DisplayPreference::SharedWriter(shared_writer) => {
+                        let tracing_writer =
+                            move || -> Box<dyn std::io::Write> { Box::new(shared_writer.clone()) };
+                        Some(Box::new(
+                            fmt_layer
+                                .with_writer(tracing_writer)
+                                .with_filter(level_filter),
+                        ))
+                    }
+                },
+                _ => None,
+            })
+        }
+
+        /// This erases the concrete type of the writer, and returns a boxed layer. This is
+        /// useful for composition of layers. There's more info in the docs
+        /// [here](https://docs.rs/tracing-subscriber/latest/tracing_subscriber/layer/index.html#runtime-configuration-with-layers).
+        pub fn layer_file<S>(
+            self,
+            level_filter: LevelFilter,
+            tracing_log_file_path_and_prefix: String,
+        ) -> miette::Result<Option<Box<DynLayer<S>>>>
+        where
+            S: tracing_core::Subscriber,
+            for<'a> S: LookupSpan<'a>,
+        {
+            // Shared configuration regardless of where logs are output to.
+            let fmt_layer = Self::create_fmt::<S>();
+
+            // Configure the writer based on the desired log target, and return it.
+            Ok(match self {
+                WriterConfig::DisplayAndFile | WriterConfig::File => {
+                    let file = rolling_file_appender_impl::try_create(
+                        tracing_log_file_path_and_prefix.as_str(),
+                    )?;
+                    Some(Box::new(
+                        fmt_layer.with_writer(file).with_filter(level_filter),
+                    ))
+                }
+                _ => None,
+            })
         }
     }
 }
 
-/// Initialize the global tracing subscriber with the given writers, level, and file path.
-///
-/// More info:
-/// - [Setup tracing](https://tokio.rs/tokio/topics/tracing)
-/// - [Configure
-///   subscriber](https://docs.rs/tracing-subscriber/latest/tracing_subscriber/fmt/index.html#configuration)
-/// - [Rolling file appender](https://docs.rs/tracing-appender/latest/tracing_appender/)
-/// - [Examples](https://github.com/tokio-rs/tracing/blob/master/examples/examples/appender-multifile.rs)
-/// - [MakeWriter](https://docs.rs/tracing-subscriber/latest/tracing_subscriber/fmt/trait.MakeWriter.html)
 pub fn init(tracing_config: TracingConfig) -> miette::Result<()> {
-    let TracingConfig {
-        writers,
-        level,
-        tracing_log_file_path_and_prefix,
-        preferred_display,
-    } = tracing_config;
+    let writer_config = match writer_config_impl::from(&tracing_config.writers) {
+        Some(it) => it,
+        None => return Ok(()),
+    };
 
-    let writer_config = WriterConfig::from(&writers);
-    if writer_config == WriterConfig::None {
-        return Ok(());
+    let level_filter = tracing_config.get_level_filter();
+
+    let layer_display =
+        writer_config.layer_display(level_filter, tracing_config.preferred_display.clone())?;
+
+    let layer_file = writer_config.layer_file(
+        level_filter,
+        tracing_config.tracing_log_file_path_and_prefix.clone(),
+    )?;
+
+    let mut layers = vec![];
+    if let Some(layer_display) = layer_display {
+        layers.push(layer_display);
+    }
+    if let Some(layer_file) = layer_file {
+        layers.push(layer_file);
     }
 
-    let builder = tracing_subscriber::fmt()
-        .compact() /* one line output */
-        // .pretty() /* multi line pretty output */
-        .with_max_level(level)
-        .without_time()
-        .with_thread_ids(true)
-        .with_thread_names(false)
-        .with_target(false)
-        .with_file(false)
-        .with_line_number(false)
-        .with_ansi(true);
-
-    match writer_config {
-        // Both file & stdout writer.
-        WriterConfig::DisplayAndFile => {
-            let writer_log = init_impl::try_create_rolling_file_appender(
-                tracing_log_file_path_and_prefix.as_str(),
-            )?
-            .with_max_level(level);
-
-            match preferred_display {
-                DisplayPreference::Stdout => {
-                    let writer_stdout =
-                        move || -> Box<dyn std::io::Write> { Box::new(std::io::stdout()) };
-                    let both = writer_log.and(writer_stdout);
-                    let subscriber = builder.with_writer(both).finish();
-                    tracing::subscriber::set_global_default(subscriber).into_diagnostic()?;
-                }
-                DisplayPreference::Stderr => {
-                    let writer_stderr =
-                        move || -> Box<dyn std::io::Write> { Box::new(std::io::stderr()) };
-                    let both = writer_log.and(writer_stderr);
-                    let subscriber = builder.with_writer(both).finish();
-                    tracing::subscriber::set_global_default(subscriber).into_diagnostic()?;
-                }
-                // Cloning a `shared_writer` makes it fail silently. This means that the
-                // tokio subscriber won't complain with `[tracing-subscriber] Unable to
-                // write an event to the Writer for this Subscriber! Error: SharedWriter
-                // Receiver has closed` if the receiver end of the channel is closed.
-                DisplayPreference::SharedWriter(shared_writer) => {
-                    let writer_shared_writer =
-                        move || -> Box<dyn std::io::Write> { Box::new(shared_writer.clone()) };
-                    let both = writer_log.and(writer_shared_writer);
-                    let subscriber = builder.with_writer(both).finish();
-                    tracing::subscriber::set_global_default(subscriber).into_diagnostic()?;
-                }
-            }
-        }
-        // Just file writer.
-        WriterConfig::File => {
-            let writer_log = init_impl::try_create_rolling_file_appender(
-                tracing_log_file_path_and_prefix.as_str(),
-            )?
-            .with_max_level(level);
-            let subscriber = builder.with_writer(writer_log).finish();
-            tracing::subscriber::set_global_default(subscriber).into_diagnostic()?;
-        }
-        // Just stdout writer.
-        WriterConfig::Display => match preferred_display {
-            DisplayPreference::Stdout => {
-                let writer_stdout =
-                    move || -> Box<dyn std::io::Write> { Box::new(std::io::stdout()) };
-                let subscriber = builder.with_writer(writer_stdout).finish();
-                tracing::subscriber::set_global_default(subscriber).into_diagnostic()?;
-            }
-            DisplayPreference::Stderr => {
-                let writer_stderr =
-                    move || -> Box<dyn std::io::Write> { Box::new(std::io::stderr()) };
-                let subscriber = builder.with_writer(writer_stderr).finish();
-                tracing::subscriber::set_global_default(subscriber).into_diagnostic()?;
-            }
-            // Cloning a `shared_writer` makes it fail silently. This means that the
-            // tokio subscriber won't complain with `[tracing-subscriber] Unable to
-            // write an event to the Writer for this Subscriber! Error: SharedWriter
-            // Receiver has closed` if the receiver end of the channel is closed.
-            DisplayPreference::SharedWriter(shared_writer) => {
-                let writer_stdout =
-                    move || -> Box<dyn std::io::Write> { Box::new(shared_writer.clone()) };
-                let subscriber = builder.with_writer(writer_stdout).finish();
-                tracing::subscriber::set_global_default(subscriber).into_diagnostic()?;
-            }
-        },
-        WriterConfig::None => {
-            unreachable!()
-        }
-    }
-
-    info!(
-        "tracing enabled {}",
-        format!(
-            "{:?}, {:?}, {:?}",
-            writers, level, tracing_log_file_path_and_prefix
-        )
-        .cyan()
-        .bold()
-    );
+    tracing_subscriber::registry().with(layers).init();
 
     Ok(())
 }
 
-mod init_impl {
+mod rolling_file_appender_impl {
     use super::*;
 
     /// Note that if you wrap this up in a non blocking writer, as shown here, it doesn't work:
     /// `tracing_appender::non_blocking(try_create_rolling_file_appender("foo")?);`
-    pub fn try_create_rolling_file_appender(
+    pub fn try_create(
         path_str: &str,
     ) -> miette::Result<tracing_appender::rolling::RollingFileAppender> {
         let path = PathBuf::from(&path_str);
@@ -233,43 +270,19 @@ mod init_impl {
     }
 }
 
-pub mod tracing_writer_config {
+#[cfg(test)]
+mod tests_writer_arg {
     use super::*;
+    use crate::WriterArg;
 
-    #[derive(Clone, Debug, PartialEq)]
-    pub enum Writer {
-        Stdout,
-        File,
-        None,
+    #[test]
+    fn test_from_str() {
+        assert_eq!(WriterArg::from_str("stdout").unwrap(), WriterArg::Stdout);
+        assert_eq!(WriterArg::from_str("file").unwrap(), WriterArg::File);
     }
 
-    impl FromStr for Writer {
-        type Err = String;
-
-        fn from_str(s: &str) -> Result<Self, Self::Err> {
-            match s {
-                "stdout" => Ok(Writer::Stdout),
-                "file" => Ok(Writer::File),
-                "none" => Ok(Writer::None),
-                "" => Ok(Writer::None),
-                _ => Err(format!("{} is not a valid tracing writer", s)),
-            }
-        }
-    }
-
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-
-        #[test]
-        fn test_from_str() {
-            assert_eq!(Writer::from_str("stdout").unwrap(), Writer::Stdout);
-            assert_eq!(Writer::from_str("file").unwrap(), Writer::File);
-        }
-
-        #[test]
-        fn test_invalid_from_str() {
-            assert!(Writer::from_str("invalid").is_err());
-        }
+    #[test]
+    fn test_invalid_from_str() {
+        assert!(WriterArg::from_str("invalid").is_err());
     }
 }
