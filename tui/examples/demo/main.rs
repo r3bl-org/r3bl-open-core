5d237fb23d57d70ec402625c047db26f04051c02
Nazmul Idris <nazmul@fasterlap.com>
2024-07-11 13:54:00 -0500

    [tui] Drop use of reedline and replace with r3bl_terminal_async

--- tui/examples/demo/main.rs
+++ tui/examples/demo/main.rs
@@ -24,9 +24,6 @@
 /// Enable debug logging.
 pub const ENABLE_TRACE_EXAMPLES: bool = true;
 
-use r3bl_rs_utils_core::*;
-use r3bl_tui::*;
-
 // Attach sources.
 mod ex_app_no_layout;
 mod ex_app_with_1col_layout;
@@ -35,30 +32,28 @@ mod ex_editor;
 mod ex_pitch;
 mod ex_rc;
 
-// Use things from sources.
-use reedline::*;
+// Use other crates.
+use std::str::FromStr;
 
-const HELP_MSG: &str = "\
-Welcome to the R3BL TUI demo app.
-Type a number to run corresponding example:
-  0. 📏 App w/ no layout
-  1. 📐 App w/ 1 column responsive layout
-  2. 📐 App w/ 2 column responsive layout
-  3. 🐒 Markdown editor, syntax highlighting, modal dialog, and emoji
-  4. ⚾ Why R3BL? Why TUI?
-  5. 📔 R3BL CMDR prototype
-
-or type Ctrl+C / Ctrl+D / 'x' to exit";
+use crossterm::style::Stylize;
+use r3bl_rs_utils_core::*;
+use r3bl_terminal_async::*;
+use r3bl_tui::*;
+use strum::IntoEnumIterator;
+use strum_macros::{Display, EnumIter, EnumString};
 
 #[tokio::main]
 async fn main() -> CommonResult<()> {
+    println!("{}", style_prompt(generate_help_msg().as_str()));
+
     throws!({
         loop {
-            let continuation = get_user_selection_from_terminal();
-            match continuation {
+            match get_user_selection_from_terminal().await? {
                 Continuation::Exit => break,
                 Continuation::Result(user_selection) => {
-                    run_user_selected_example(user_selection).await?;
+                    if run_user_selected_example(user_selection).await.is_err() {
+                        break;
+                    };
                 }
                 _ => {}
             }
@@ -66,40 +61,132 @@ async fn main() -> CommonResult<()> {
     })
 }
 
-async fn run_user_selected_example(selection: String) -> CommonResult<()> {
-    throws!({
-        if !selection.is_empty() {
-            match selection.as_ref() {
-                "0" => throws!(ex_app_no_layout::launcher::run_app().await?),
-                "1" => throws!(ex_app_with_1col_layout::launcher::run_app().await?),
-                "2" => throws!(ex_app_with_2col_layout::launcher::run_app().await?),
-                "3" => throws!(ex_editor::launcher::run_app().await?),
-                "4" => throws!(ex_pitch::launcher::run_app().await?),
-                "5" => throws!(ex_rc::launcher::run_app().await?),
-                _ => eprintln!("{}", style_error("Unknown selection 🤷")),
-            }
-        }
-    })
-}
+async fn get_user_selection_from_terminal() -> CommonResult<Continuation<String>> {
+    let maybe_terminal_async = TerminalAsync::try_new("> ").await?;
 
-/// This is a single threaded blocking function. The R3BL examples are all async and non-blocking.
-fn get_user_selection_from_terminal() -> Continuation<String> {
-    println!("{}", style_prompt(HELP_MSG));
+    // If the terminal is not fully interactive, then return early.
+    let Some(mut terminal_async) = maybe_terminal_async else {
+        return Ok(Continuation::Exit);
+    };
 
-    let mut line_editor = Reedline::create();
-    let prompt = DefaultPrompt::default();
+    // Pre-populate the readline's history with some entries.
+    for command in AutoCompleteCommand::iter() {
+        terminal_async
+            .readline
+            .add_history_entry(command.to_string());
+    }
 
     loop {
-        let maybe_signal = &line_editor.read_line(&prompt);
-        if let Ok(Signal::Success(user_input_text)) = maybe_signal {
-            match user_input_text.as_str() {
-                "x" => break,
-                _ => return Continuation::Result(user_input_text.into()),
+        let result_user_input = terminal_async.get_readline_event().await;
+        match result_user_input {
+            Ok(user_input) => match user_input {
+                ReadlineEvent::Line(input) => return Ok(Continuation::Result(input)),
+                ReadlineEvent::Eof => break,
+                ReadlineEvent::Interrupted => break,
+                _ => {}
+            },
+            Err(_) => {
+                break;
             }
-        } else if let Ok(Signal::CtrlC) | Ok(Signal::CtrlD) = maybe_signal {
-            break;
         }
     }
 
-    Continuation::Exit
+    Ok(Continuation::Exit)
+}
+/// You can type both "0" or "App with no layout" to run the first example. Here are some
+/// details:
+/// - `selection` is what the user types in the terminal, eg: "0" or "App with no layout".
+/// - result_command is the parsed command from the selection, eg:
+///   [AutoCompleteCommand::NoLayout].
+async fn run_user_selected_example(selection: String) -> CommonResult<()> {
+    let result_command /* Eg: Ok(Exit) */ =
+        AutoCompleteCommand::from_str(&selection /* eg: "0" */);
+    match result_command {
+        Ok(command) => match command {
+            AutoCompleteCommand::NoLayout => {
+                throws!(ex_app_no_layout::launcher::run_app().await?)
+            }
+            AutoCompleteCommand::OneColLayout => {
+                throws!(ex_app_with_1col_layout::launcher::run_app().await?)
+            }
+            AutoCompleteCommand::TwoColLayout => {
+                throws!(ex_app_with_2col_layout::launcher::run_app().await?)
+            }
+            AutoCompleteCommand::Editor => {
+                throws!(ex_editor::launcher::run_app().await?)
+            }
+            AutoCompleteCommand::Slides => {
+                throws!(ex_pitch::launcher::run_app().await?)
+            }
+            AutoCompleteCommand::Commander => {
+                throws!(ex_rc::launcher::run_app().await?)
+            }
+            AutoCompleteCommand::Exit => CommonError::new_err_with_only_msg("Exiting..."),
+        },
+        Err(_) => {
+            println!("{} {}", "Invalid selection:".blue(), selection.red().bold());
+            Ok(())
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, EnumString, EnumIter, Display)]
+enum AutoCompleteCommand {
+    #[strum(ascii_case_insensitive)]
+    #[strum(to_string = "App with no layout")]
+    #[strum(serialize = "0")]
+    NoLayout,
+
+    #[strum(ascii_case_insensitive)]
+    #[strum(to_string = "App with 1 column responsive layout")]
+    #[strum(serialize = "1")]
+    OneColLayout,
+
+    #[strum(ascii_case_insensitive)]
+    #[strum(to_string = "App with 2 column responsive layout")]
+    #[strum(serialize = "2")]
+    TwoColLayout,
+
+    #[strum(ascii_case_insensitive)]
+    #[strum(to_string = "Markdown editor, syntax highlighting, modal dialog, and emoji")]
+    #[strum(serialize = "3")]
+    Editor,
+
+    #[strum(ascii_case_insensitive)]
+    #[strum(to_string = "Why R3BL? Why TUI?")]
+    #[strum(serialize = "4")]
+    Slides,
+
+    #[strum(ascii_case_insensitive)]
+    #[strum(to_string = "R3BL CMDR prototype")]
+    #[strum(serialize = "5")]
+    Commander,
+
+    #[strum(ascii_case_insensitive)]
+    #[strum(to_string = "Exit")]
+    #[strum(serialize = "x")]
+    Exit,
+}
+
+fn generate_help_msg() -> String {
+    format!(
+        "\
+Welcome to the R3BL TUI demo app.
+Type a number to run corresponding example:
+  0. 📏 {}
+  1. 📐 {}
+  2. 📐 {}
+  3. 🐒 {}
+  4. ⚾ {}
+  5. 📔 {}
+
+or type Ctrl+C / Ctrl+D / '{}' to exit",
+        AutoCompleteCommand::NoLayout,
+        AutoCompleteCommand::OneColLayout,
+        AutoCompleteCommand::TwoColLayout,
+        AutoCompleteCommand::Editor,
+        AutoCompleteCommand::Slides,
+        AutoCompleteCommand::Commander,
+        AutoCompleteCommand::Exit,
+    )
 }
