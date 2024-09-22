0d7fbe1560ba74b12f2b047fc4db4f2357699e62
Nazmul Idris <nazmul@fasterlap.com>
2024-09-21 19:42:56 -0500

    [core][tui] Move pretty print traits from tui into core

--- tui/src/tui/misc_types.rs
+++ tui/src/tui/misc_types.rs
@@ -25,8 +25,8 @@ pub use editor_component_traits::*;
 pub use global_constants::*;
 pub use list_of::*;
 pub use misc_type_aliases::*;
+pub use pretty_print::*;
 pub use pretty_print_option::*;
-pub use pretty_print_traits::*;
 use r3bl_rs_utils_core::*;
 use strum_macros::AsRefStr;
 
@@ -278,62 +278,6 @@ mod cli_args {
             let items = my_vec.iter().map(String::as_str).collect::<Vec<&str>>();
             List { inner: items }
         }
-    }
-}
-
-mod pretty_print_traits {
-    use crossterm::style::Stylize;
-
-    use crate::US;
-
-    /// Marker trait to "remember" which types can be printed to the console w/ color.
-    pub trait ConsoleLogInColor {
-        fn console_log_fg(&self);
-        fn console_log_bg(&self);
-    }
-
-    fn console_log_fg(this: &str) {
-        if this.is_empty() {
-            println!("\n{}", "← empty →".yellow());
-        } else {
-            println!("\n{}", this.yellow());
-        }
-    }
-
-    fn console_log_bg(this: &str) {
-        if this.is_empty() {
-            println!("\n{}", "← empty →".red().on_white());
-        } else {
-            println!("\n{}", this.red().on_white());
-        }
-    }
-
-    impl<T: PrettyPrintDebug> ConsoleLogInColor for T {
-        fn console_log_fg(&self) { console_log_fg(&self.pretty_print_debug()); }
-
-        fn console_log_bg(&self) { console_log_bg(&self.pretty_print_debug()); }
-    }
-
-    impl ConsoleLogInColor for &str {
-        fn console_log_fg(&self) { console_log_fg(self); }
-
-        fn console_log_bg(&self) { console_log_bg(self); }
-    }
-
-    impl ConsoleLogInColor for String {
-        fn console_log_fg(&self) { console_log_fg(self); }
-
-        fn console_log_bg(&self) { console_log_bg(self); }
-    }
-
-    /// Marker trait to "remember" which types support pretty printing for debugging.
-    pub trait PrettyPrintDebug {
-        fn pretty_print_debug(&self) -> String;
-    }
-
-    /// Marker trait to "remember" which types can be converted to plain text.
-    pub trait ConvertToPlainText {
-        fn to_plain_text_us(&self) -> US;
     }
 }
 
