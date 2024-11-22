51a599d60df25b4b2a9268b0e37021597e16b6cb
Nazmul Idris <nazmul@fasterlap.com>
2024-11-22 08:30:58 -0600

    [test_fixtures] Add fixtures to mock sockets

--- test_fixtures/src/tcp_stream_fixtures/mock_async_stream.rs
+++ test_fixtures/src/tcp_stream_fixtures/mock_async_stream.rs
@@ -23,19 +23,20 @@ use tokio::io::{AsyncRead, AsyncWrite};
 
 /// A mock struct for the [tokio::net::TcpStream].
 /// - Alternative to [tokio_test::io::Builder].
-/// - The difference is that [MockTcpStream] allows access to the expected write buffer.
-pub struct MockTcpStream {
-    pub expected_write: Vec<u8>,
+/// - The difference is that [MockAsyncStream] allows access to the expected write buffer.
+pub struct MockAsyncStream {
+    pub expected_buffer: Vec<u8>,
 }
 
-/// Implement the [AsyncWrite] trait for the mock struct.
-impl AsyncWrite for MockTcpStream {
+/// Implement the [AsyncWrite] trait for the mock struct. This struct also automatically
+/// implements [Unpin], because it contains no self-referencing pointers.
+impl AsyncWrite for MockAsyncStream {
     fn poll_write(
         mut self: Pin<&mut Self>,
         _cx: &mut Context<'_>,
         buf: &[u8],
     ) -> Poll<Result<usize>> {
-        self.expected_write.extend_from_slice(buf);
+        self.expected_buffer.extend_from_slice(buf);
         Poll::Ready(Ok(buf.len()))
     }
 
@@ -48,17 +49,18 @@ impl AsyncWrite for MockTcpStream {
     }
 }
 
-/// Implement the [AsyncRead] trait for the mock struct.
-impl AsyncRead for MockTcpStream {
+/// Implement the [AsyncRead] trait for the mock struct. This struct also automatically
+/// implements [Unpin], because it contains no self-referencing pointers.
+impl AsyncRead for MockAsyncStream {
     fn poll_read(
         mut self: Pin<&mut Self>,
         _cx: &mut Context<'_>,
         buf: &mut tokio::io::ReadBuf<'_>,
     ) -> std::task::Poll<std::result::Result<(), std::io::Error>> {
-        let data = self.expected_write.as_slice();
+        let data = self.expected_buffer.as_slice();
         let len = std::cmp::min(data.len(), buf.remaining());
         buf.put_slice(&data[..len]);
-        self.expected_write.drain(..len);
+        self.expected_buffer.drain(..len);
         Poll::Ready(Ok(()))
     }
 }
