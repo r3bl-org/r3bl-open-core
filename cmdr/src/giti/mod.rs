8f4c169984ee022c0e4ec737d60dc2f9ba1f9542
Nazmul Idris <nazmul@fasterlap.com>
2025-04-08 11:56:04 -0500

    [cmdr][giti] Remove Reedline and use ReadlineAsync instead
    - Prune all the needless deps in all the Cargo.toml files in the
      workspace
    - Start refactoring the giti codebase for readability

--- cmdr/src/giti/mod.rs
+++ cmdr/src/giti/mod.rs
@@ -20,9 +20,11 @@ pub mod branch;
 pub mod clap_config;
 pub mod common_types;
 pub mod ui_strings;
+pub mod ui_templates;
 
 // Re-export.
 pub use branch::*;
 pub use clap_config::*;
 pub use common_types::*;
 pub use ui_strings::*;
+pub use ui_templates::*;
