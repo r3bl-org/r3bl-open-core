5fd3da191aa17d590849762940b6cec78a59b096
Nazmul Idris <nazmul@fasterlap.com>
2025-04-09 14:43:59 -0500

    [cmdr][giti] Rewrite the code so it is modular and readable

--- cmdr/src/giti/branch/new.rs
+++ cmdr/src/giti/branch/new.rs
@@ -29,11 +29,7 @@ use r3bl_tui::{ReadlineAsync, ReadlineEvent};
 use crate::giti::{self,
                   SuccessReport,
                   clap_config::BranchSubcommand,
-                  ui_strings::UIStrings::{BranchAlreadyExists,
-                                          CreatedAndSwitchedToNewBranch,
-                                          EnterBranchNameYouWantToCreate,
-                                          FailedToCreateAndSwitchToBranch,
-                                          NoNewBranchWasCreated},
+                  ui_strings::UIStrings,
                   ui_templates::report_unknown_error_and_propagate};
 
 pub async fn try_make_new_branch(
@@ -53,19 +49,21 @@ fn success_report() -> CommonResult<SuccessReport> {
 }
 
 fn handle_branch_creation(branch_name: String) -> CommonResult<SuccessReport> {
-    let branches = giti::get_branches()?;
+    let branches = giti::try_get_local_branches()?;
     let branches_trimmed: Vec<String> = branches
         .iter()
         .map(|branch| branch.trim_start_matches("(current) ").to_string())
         .collect();
 
     if branches_trimmed.contains(&branch_name) {
-        fg_slate_gray(&BranchAlreadyExists { branch_name }.to_string()).println();
+        fg_slate_gray(&UIStrings::BranchAlreadyExists { branch_name }.to_string())
+            .println();
         return success_report();
     }
 
     let git_command =
         &mut create_git_command_to_create_and_switch_to_branch(&branch_name);
+
     match git_command.output() {
         Ok(output) if output.status.success() => {
             display_successful_new_branch_creation(&branch_name)
@@ -84,7 +82,8 @@ fn handle_branch_creation(branch_name: String) -> CommonResult<SuccessReport> {
 
 async fn prompt_for_branch_name() -> CommonResult<SuccessReport> {
     let prompt_text =
-        fg_frozen_blue(&EnterBranchNameYouWantToCreate.to_string()).to_string();
+        fg_frozen_blue(&UIStrings::EnterBranchNameYouWantToCreate.to_string())
+            .to_string();
     let mut rl_async = ReadlineAsync::try_new(Some(&prompt_text))?
         .ok_or_else(|| miette::miette!("Failed to create terminal"))?;
 
@@ -98,7 +97,8 @@ async fn prompt_for_branch_name() -> CommonResult<SuccessReport> {
             }
             ReadlineEvent::Eof | ReadlineEvent::Interrupted => {
                 rl_async.exit(None).await.into_diagnostic()?;
-                fg_silver_metallic(&NoNewBranchWasCreated.to_string()).println();
+                fg_silver_metallic(&UIStrings::NoNewBranchWasCreated.to_string())
+                    .println();
                 return success_report();
             }
             ReadlineEvent::Resized => { /* Do nothing */ }
@@ -108,7 +108,7 @@ async fn prompt_for_branch_name() -> CommonResult<SuccessReport> {
 
 fn display_failed_to_create_new_branch(branch_name: &str) {
     fg_guards_red(
-        &FailedToCreateAndSwitchToBranch {
+        &UIStrings::FailedToCreateAndSwitchToBranch {
             branch_name: branch_name.to_string(),
         }
         .to_string(),
@@ -119,7 +119,7 @@ fn display_failed_to_create_new_branch(branch_name: &str) {
 fn display_successful_new_branch_creation(branch_name: &str) {
     println!(
         "{a}{b}",
-        a = fg_slate_gray(&CreatedAndSwitchedToNewBranch.to_string()),
+        a = fg_slate_gray(&UIStrings::CreatedAndSwitchedToNewBranch.to_string()),
         b = fg_lizard_green(&format!("✅ {branch_name}"))
     );
 }
