0a6a77e73f32be83b74e84f07dab3067e2be76d4
Nazmul Idris <nazmul@fasterlap.com>
2025-05-01 16:30:45 -0500

    [cmdr] Update docker file to test r3bl-cmdr install
    Since `openssl` is dropped, by using different feature flags for `reqwest`
    drop the requirement to have `libssl-dev` & `pck-config` installed on the
    machine that runs `cargo install r3bl-cmdr`

--- cmdr/docker/install.bash
+++ cmdr/docker/install.bash
@@ -19,7 +19,7 @@
 
 apt-get update -y
 apt-get upgrade -y
-apt-get install -y curl gcc build-essential libssl-dev pkg-config
+apt-get install -y curl gcc build-essential
 
 # More info: https://rust-lang.github.io/rustup/installation/index.html
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
@@ -28,3 +28,5 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 cargo install r3bl-cmdr
 
 edi --version
+giti --version
+rc --version
