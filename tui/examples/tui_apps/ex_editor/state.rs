55d5d9e3c2af53a402d6db4d76877844d62cd1f9
Nazmul Idris <nazmul@fasterlap.com>
2025-06-26 22:22:16 -0500

    WIP 74 - Clean up compatibility_test_suite.rs
    - Remove needless tests
    - Consolidate real world editor markdown content in
      md_parser_ng::get_real_world_editor_content()
      Both compatibility test suite and examples in
      tui_apps/ex_editor/state.rs now use the same content to test on
      and run the example with

--- tui/examples/tui_apps/ex_editor/state.rs
+++ tui/examples/tui_apps/ex_editor/state.rs
@@ -18,7 +18,8 @@
 use std::{collections::HashMap,
           fmt::{Debug, Formatter, Result}};
 
-use r3bl_tui::{DialogBuffer,
+use r3bl_tui::{get_real_world_editor_content,
+               DialogBuffer,
                EditorBuffer,
                FlexBoxId,
                HasDialogBuffers,
@@ -45,7 +46,7 @@ mod constructor {
             let editor_buffer = {
                 let mut editor_buffer =
                     EditorBuffer::new_empty(Some(DEFAULT_SYN_HI_FILE_EXT), None);
-                let iter = get_default_content().iter().copied();
+                let iter = get_real_world_editor_content().iter().copied();
                 editor_buffer.set_lines(iter);
                 editor_buffer
             };
@@ -58,69 +59,6 @@ mod constructor {
             editor_buffers,
             dialog_buffers: Default::default(),
         }
-    }
-
-    pub fn get_default_content<'a>() -> &'a [&'a str] {
-        &[
-"0         1         2         3         4         5         6",
-"0123456789012345678901234567890123456789012345678901234567890",
-"@title: untitled",
-"@tags: foo, bar, baz",
-"@authors: xyz, abc",
-"@date: 12-12-1234",
-"",
-"# This approach will not be easy. You are required to fly straight😀",
-"## Did he take those two new droids with him? They hit accelerator.😀 We will deal with your Rebel friends. Commence primary ignition.😀",
-"",
-"1. line 1 of 2",
-"2. line 2 of 2",
-"",
-"This is _not italic [link](https://r3bl.com) not bold* etc.",
-"",
-"```ts",
-"let a=1;",
-"```",
-"",
-"`foo`",
-"",
-"*bar*",
-"**baz**",
-"",
-"```rs",
-"let a=1;",
-"```",
-"",
-"- [x] done",
-"- [ ] todo",
-"",
-"# Random writing from star wars text lorem ipsum generator",
-"",
-"1. A hyperlink [link](https://forcemipsum.com/)",
-"   inline code `code`",
-"    2. Did you hear that?",
-"       They've shut down the main reactor.",
-"       We'll be destroyed for sure.",
-"       This is madness!",
-"       We're doomed!",
-"",
-"## Random writing from star trek text lorem ipsum generator",
-"",
-"- Logic is the beginning of wisdom, not the end. ",
-"  A hyperlink [link](https://fungenerators.com/lorem-ipsum/startrek/)",
-"  I haven't faced death. I've cheated death. ",
-"  - I've tricked my way out of death and patted myself on the back for my ingenuity; ",
-"    I know nothing. It's not safe out here. ",
-"    - Madness has no purpose. Or reason. But it may have a goal.",
-"      Without them to strengthen us, we will weaken and die. ",
-"      You remove those obstacles.",
-"      - But one man can change the present!  Without freedom of choice there is no creativity. ",
-"        I object to intellect without discipline; I object to power without constructive purpose. ",
-"        - Live Long and Prosper. To Boldly Go Where No Man Has Gone Before",
-"          It’s a — far, far better thing I do than I have ever done before",
-"          - A far better resting place I go to than I have ever know",
-"            Something Spock was trying to tell me on my birthday",
-"",
-        ]
     }
 }
 
